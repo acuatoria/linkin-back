a9398a1525b2e2aabc70fdb139a55fa65b7c14b9
Teo <acuatoria@gmail.com>
2023-08-27 11:17:10 +0200

    load test

--- linkin/urls.py
+++ linkin/urls.py
@@ -1,9 +1,10 @@
 
-from django.conf import settings
-from django.urls import path, re_path, include, reverse_lazy
-from django.conf.urls.static import static
 from django.contrib import admin
+from django.conf import settings
+from django.http import HttpResponse
+from django.conf.urls.static import static
 from django.views.generic.base import RedirectView
+from django.urls import path, re_path, include, reverse_lazy
 
 from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken import views
@@ -34,6 +35,7 @@ urlpatterns = [
     path('api/v1/', include(router.urls)),
     path('api-token-auth/', views.obtain_auth_token),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('loaderio-c16a286f8471124641c4f0cfb17b67a7.txt', lambda _: HttpResponse(open('loaderio-c16a286f8471124641c4f0cfb17b67a7.txt', 'r'), content_type='text/plain')),
 
     # the 'api-root' from django rest-frameworks default router
     # http://www.django-rest-framework.org/api-guide/routers/#defaultrouter
